c2f687ebefc90a6b3e078ae2935e1a96aaa870d8
johanstahre <Johan@bk30.se>
2020-09-18 20:49:38 +0200

    added 2 buttons and start of onClick functions

--- app/src/main/java/com/example/blackjack/GameActivity.kt
+++ app/src/main/java/com/example/blackjack/GameActivity.kt
@@ -2,10 +2,50 @@ package com.example.blackjack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
+    val log ="!!!"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
     }
+
+
+    //start of game
+
+
+
+
+    //function for when player presses Hit
+    fun playerHit(view : View){
+        Log.d(log, "Player Hit")
+
+        //Add a card to players Hand
+
+    }
+
+
+
+    //function for when player presses Pass
+    fun playerPass(view : View){
+        Log.d(log, "player Pass")
+        
+        buttonHit.isClickable = false
+        buttonHit.visibility = View.GONE
+
+        buttonPass.isClickable = false
+        buttonPass.visibility = View.GONE
+
+
+        //do stuff depending on Dealers Hand
+    }
+
+
+
+
 }
