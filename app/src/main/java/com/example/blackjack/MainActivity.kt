aeb3582f0b43fc5da7023cc8be9f876f9e9565a5
johanstahre <Johan@bk30.se>
2020-09-18 15:41:25 +0200

    Created GameaCtivity and added it to onClick button

--- app/src/main/java/com/example/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/blackjack/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.blackjack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //Changes activity to GameActivity
+    fun newGameButton(view : View){
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 }
